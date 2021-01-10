fa07229c2e0e2ac1f28ac87cb8052ab2595d3836
rsingh859 <61348206+rsingh859@users.noreply.github.com>
2021-01-11 00:31:50 +0530

    Custom FormInput and Buttons

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import HomePage from './pages/homepage/homepage.component';
 import { Route, Switch } from 'react-router-dom';
 import ShopPage from './pages/shop-page/shop-page.components';
 import Header from './components/header/header.component';
+import SignInSignUp from './pages/sign-in-sign-up/sign-in-sign-up.component';
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
     <Switch>
       <Route exact path='/' component={HomePage} />
       <Route path='/shop' component={ShopPage} />
+      <Route path='/signin' component={SignInSignUp} />
     </Switch>
     </div>
   );
