475748613d9b5b2977750c7518ad6f195c265caf
rsingh859 <61348206+rsingh859@users.noreply.github.com>
2021-01-13 21:24:54 +0530

    Creating in-memory state for cart components using reselector

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,25 +1,34 @@
 import React from 'react';
-import { connect } from 'react-redux';
+//import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 import CustomButton from '../custom-button/custom-button.component';
+import { selectCartItems } from '../../redux/cart/cart.selectors';
 
 import CartItem from '../cart-item/cart-item.component';
 
 import './cart-dropdown.styles.scss';
 
-const cartDropDown = ({ cartItems }) => (
-    <div className='cart-dropdown'>
-        <div className='cart-items'>
-            {
-                cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem} />)
-            }
+const CartDropDown = () =>{
+
+    const {cartItems} = useSelector((state) => ({ cartItems: selectCartItems(state) }));
+
+    return(
+        <div className='cart-dropdown'>
+            <div className='cart-items'>
+                {
+                    cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem} />)
+                }
+            </div>
+            <CustomButton>GO TO CHECKOUT</CustomButton>
         </div>
-        <CustomButton>GO TO CHECKOUT</CustomButton>
-    </div>
-);
+    );
+}
 
-const mapStateToProps = ({ cart: {cartItems } }) =>({
-    cartItems
-});
+export default CartDropDown;
 
-export default connect(mapStateToProps)(cartDropDown);
+// const mapStateToProps = ({ cart: {cartItems } }) =>({
+//     cartItems
+// });
+
+// export default connect(mapStateToProps)(cartDropDown);
--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,18 +1,31 @@
 import React from 'react';
 import './cart-icon.styles.scss';
-import { connect } from 'react-redux';
+//import { connect } from 'react-redux';
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
+import { useDispatch,useSelector } from 'react-redux';
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 
-const CartIcon = ({ toggleCartHidden }) => (
-    <div className='cart-icon' onClick={ toggleCartHidden }>
+const CartIcon = () => {
+    
+    const {itemCount} = useSelector((state) => ({
+        itemCount: selectCartItemsCount(state)
+    }), () => console.log('I am being called'));
+
+    const dispatch = useDispatch();
+    
+    return(
+    <div className='cart-icon' onClick={() => dispatch(toggleCartHidden()) }>
         <ShoppingIcon className='shopping-icon' />
-        <span className='item-count'>0</span> 
+        <span className='item-count'>{itemCount}</span> 
     </div>
-);
+    );    
+};
 
-const mapDispatchToProps = dispatch => ({
-    toggleCartHidden: () => dispatch(toggleCartHidden())
-});
+// const mapDispatchToProps = dispatch => ({
+//     toggleCartHidden: () => dispatch(toggleCartHidden())
+// });
 
-export default connect(null,mapDispatchToProps)(CartIcon);
+//export default connect(null,mapDispatchToProps)(CartIcon);
+
+export default CartIcon;
