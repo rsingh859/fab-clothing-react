c5d2e6155bfdb751a37314ba3f52bea9cba1c3ab
rsingh859 <61348206+rsingh859@users.noreply.github.com>
2021-01-20 00:21:28 +0530

    Optimizing production build

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,12 @@ import { logger } from 'redux-logger';
 import rootReducer from './root-reducer';
 import { persistStore } from 'redux-persist';
 
-const middleware = [logger];
+const middleware = [];
+
+if(process.env.NODE_ENV == 'development') {
+    middleware.push(logger);
+}
+
 const redux_tool = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 
 export const store = createStore(rootReducer, compose(applyMiddleware(...middleware), redux_tool));
