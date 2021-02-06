c8bbc890514ab6f4b1343a18d282abca01641617
rsingh859 <61348206+rsingh859@users.noreply.github.com>
2021-02-06 23:06:23 +0530

    PWA complaint

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,8 @@ import { Provider } from 'react-redux';
 import { store, persistor } from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
 
+import * as serviceWorker from './serviceWorker';
+
 ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
@@ -19,3 +21,5 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
+
+serviceWorker.register();
